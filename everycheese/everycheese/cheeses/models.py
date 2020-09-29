0c8f7a13a30fea192ffba048729e0f8b9b9c0eb9
Michael Martyn <michael.martyn.92@gmail.com>
2020-09-28 20:25:20 -0700

    Add cheeses app

---
+++ everycheese/everycheese/cheeses/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+from autoslug import AutoSlugField
+from model_utils.models import TimeStampedModel
+
+
+class Cheese(TimeStampedModel):
+    """TimeStampedModel automatically gives the model 'created' and
+    'modified' fields, which automatically track when the object is
+    created or modified."""
+
+    def __str__(self):
+        return self.name
+
+    class Firmness(models.TextChoices):
+        """Note that we defined the firmness constants as variables within
+        the scope of the Cheese model. This allows for direct comparisons."""
+        UNSPECIFIED = "unspecified", "Unspecified"
+        SOFT = "soft", "Soft"
+        SEMI_SOFT = "semi_soft", "Semi-Soft"
+        SEMI_HARD = "semi-hard", "Semi-Hard"
+        HARD = "hard", "Hard"
+
+    name = models.CharField("Name of cheese", max_length=255)
+    slug = AutoSlugField("Cheese Address",
+        unique=True, always_update=False, populate_from="name")
+
+    # A good rule of thumb is to use TextField rather than CharField whenever
+    # there might be a need for more than 255 characters.
+    description = models.TextField("Description", blank=True)
+
+    firmness = models.CharField("Firmness", max_length=20,
+        choices=Firmness.choices, default=Firmness.UNSPECIFIED)
