6d795258dc4d480e203e0b376b9c883ca612050c
Michael Martyn <michael.martyn.92@gmail.com>
2020-10-02 16:49:31 -0700

    Track and display creator of each cheese

--- everycheese/everycheese/cheeses/views.py
+++ everycheese/everycheese/cheeses/views.py
@@ -14,9 +14,12 @@ class CheeseDetailView(DetailView):
 # next step is to add the corresponding URL in cheeses/urls.py
 class CheeseCreateView(LoginRequiredMixin, CreateView):
     model = Cheese
-    fields = [
-        'name',
-        'description',
-        'country_of_origin',
-        'firmness',
-    ]
+    fields = ['name', 'description', 'country_of_origin', 'firmness',]
+
+    def form_valid(self, form):
+        """Override the default form_valid method to insert creator field
+            after the form has been validated. There is no need to
+            validate the creator since it is assigned by our code, not
+            a form input by the User."""
+        form.instance.creator = self.request.user
+        return super().form_valid(form)
